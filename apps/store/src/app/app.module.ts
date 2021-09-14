3554f314cc3b1eaa9d5de2c25c508b9656f3934d
neil.naidu <neil.naidu@investec.co.za>
2021-09-14 19:30:54 +0200

    feat: up to lab 4

---
+++ apps/store/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { UiSharedModule } from './../../../../libs/ui-shared/src/lib/ui-shared.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    UiSharedModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ libs/ui-shared/src/lib/header/header.component.ts
@@ -0,0 +1,16 @@
+import { UiSharedModule } from './../ui-shared.module';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bg-hoard-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
