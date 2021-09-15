9286eda720001ecc70be451d0bdd10f7cb26ff00
neil.naidu <neil.naidu@investec.co.za>
2021-09-15 18:41:18 +0200

    feat: lab 13 - nothing runs

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,10 +1,19 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+export default async function (tree: Tree, schema: Schema) {
+  await libraryGenerator(tree, 
+    {name: `util-${schema.name}`,
+  directory: schema.directory,
+  tags: `type:util, scope:${schema.directory}`
+});
   await formatFiles(tree);
   return () => {
+    // will not be run when passing --dry-run
     installPackagesTask(tree);
   };
+}
+interface Schema {
+  name: string;
+  directory: 'store' | 'api' | 'shared';
 }
