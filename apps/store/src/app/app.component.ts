3d4307cab4c9aa7fcb7a92f52d6417d29985d156
neil.naidu <neil.naidu@investec.co.za>
2021-09-15 22:26:18 +0200

    feat: 16 nx cloud

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { formatRating } from '@bg-hoard/store/util-formatters';
+import { formatRating } from '@bg-hoard/util-formatters';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor(private http: HttpClient) {}
--- libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { map, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
-import { formatRating } from '@bg-hoard/store/util-formatters';
-import { Game } from '@bg-hoard/util-interface';
+import { formatRating } from '@bg-hoard/util-formatters';
+import { Game } from 'util-interface';
 
 @Component({
   selector: 'bg-hoard-game-detail',
   templateUrl: './game-detail.component.html',
-  styleUrls: ['./game-detail.component.css']
+  styleUrls: ['./game-detail.component.scss']
 })
 export class GameDetailComponent {
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
