d40b8fb25b5d61e818b49ff7db76cc1f25fdf00f
neil.naidu <neil.naidu@investec.co.za>
2021-09-15 17:41:38 +0200

    feat: lib 12

--- libs/ui-shared/src/lib/ui-shared.module.ts
+++ libs/ui-shared/src/lib/ui-shared.module.ts
@@ -1,8 +1,8 @@
+//import { StoreFeatureGameDetailModule } from './../../../store/feature-game-detail/src/lib/store-feature-game-detail.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 
 @NgModule({
   imports: [CommonModule, MatToolbarModule],
